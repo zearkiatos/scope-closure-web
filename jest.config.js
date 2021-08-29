8d93425208148f19c248fa636ce563d2c384d3b3
zearkiatos <caprilespe@outlook.com>
2021-08-29 14:41:40 -0400

    Fix code coverage

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
     collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**"],
     coverageThreshold: {
       global: {
-        statements: 50,
-        branches: 50,
-        lines: 50,
-        functions: 50,
+        statements: 89,
+        branches: 33,
+        lines: 88,
+        functions: 95,
       },
     },
   };
